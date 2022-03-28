62eda39cb52bbe5f1bbb7c9bd1988d649fa13473
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-28 09:51:53 +0200

    Create Built-in function.py

---
+++ Built-in function.py
@@ -0,0 +1,6 @@
+# Built-in functions or predefined functions are: range(), print(), input() ect.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+print("Hello world!")
