bbca9c15df3c98965881e2bd0786d3dc4bc3c7bd
Ongl sy'n cwympo <55692485+Ongl-syn-cwympo@users.noreply.github.com>
2022-03-28 09:51:50 +0200

    Create User-defined function.py

---
+++ User-defined function.py
@@ -0,0 +1,7 @@
+# Functions which are created by programmer explicitly according to the requirement.
+# Python code 
+# Created by Ongl syn cwympo 
+# https://github.com/Ongl-syn-cwympo
+
+def fun():
+    print("Please write something here inside of fun()")
